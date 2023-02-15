204a33a7f2e899e7b7afd06b1b49d3164d361e04
Ashley Hoyt <turnerashley574@gmail.com>
2023-02-15 15:35:09 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const Header = (props) =>{
+  let greeting = props.greeting;
+  
+
+  return(
+    <header><h1 className= 'title'>{greeting}</h1></header>
+  );
+}
+
+const CurrentTime = () =>{
+
+ const [time, setTime] = useState(new Date);
+ const [ampm, setAmpm] = useState("");
+ const [date, theDate] = useState(new Date);
+ 
+
+ 
+ 
+ useEffect(() =>{
+
+  const now = setInterval(()=>{
+    setTime(new Date);
+    if(time.getHours() >= 12){
+      setAmpm("PM");
+    }
+    else{
+      setAmpm("AM");
+    }
+  }, 1000);
+  return () => clearInterval(now);
+
+ }, []
+ );
+
+useEffect (() => {
+  const date = setInterval(() =>{
+    theDate(new Date);
+  }, 1000);
+});
+
+ return(
+<div>
+  <h1>{time.getHours()>12?time.getHours()-12:time.getHours()} : {time.getMinutes()} {ampm} </h1>
+  <h1> {date.toLocaleDateString()} </h1>
+  </div>
+ )
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header greeting= "Current Time: "/>
+    
+      <CurrentTime />
+
     </div>
   );
 }
